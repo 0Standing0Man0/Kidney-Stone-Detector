340401d1fee0c026ac48178c05aaf5ef407b808b
adrijamajumder3 <130859357+adrijamajumder3@users.noreply.github.com>
2024-03-10 00:38:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function uploadImage() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            const filePath = event.target.result;
+            const link = document.createElement('a');
+            link.href = filePath;
+            link.download = file.name;
+            link.click();
+            URL.revokeObjectURL(filePath);
+        };
+        reader.readAsDataURL(file);
+    } else {
+        alert('Please select an image to upload.');
+    }
+}
